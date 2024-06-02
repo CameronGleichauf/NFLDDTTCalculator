69105f0a418de27620ddbc649c610a051aa470c3
Cameron Gleichauf <66345385+CameronGleichauf@users.noreply.github.com>
2024-06-02 12:24:32 -0400

    routes.rb refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,20 @@
 Rails.application.routes.draw do
-  get 'trades/index'
-  get 'trades/new'
-  get 'trades/create'
-  get 'trades/show'
-  get 'picks/index'
-  get 'picks/show'
+  post '/trades/save_trade', to: 'trades#save_trade'
+  delete 'teams/:id/clear_trades', to: 'teams#clear_trades', as: 'clear_trades'
+  resources :trades, only: [:index, :new, :create, :show] do
+    post 'save_trade', on: :collection # Custom route for saving trade
+  end
 
   resources :picks, only: [:index, :show]
 
   resources :teams, only: [:index, :show] do
-    resources :picks, only: [:index]
+    resources :picks, only: [:index], on: :collection # Nested picks route for teams
+    delete 'clear_trades', on: :member # Custom route to clear trades for a team
   end
-  resources :trades, only: [:index, :new, :create, :show]
 
   get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
   root "home#index"
-
-  get "/teams", to: "teams#index"
-  get "/teams/:id", to: "teams#show"
-
-  post '/trades/save_trade', to: 'trades#save_trade'
-  delete 'teams/:id/clear_trades', to: 'teams#clear_trades', as: 'clear_trades'
 end
